e5cd2314250926a2f5348a2f699baf36c23033de
James <madcat4623@gmail.com>
2025-06-17 23:27:44 +0200

    Login utente

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const initDb = require('./initDb');
+const initDb = require('./db/initDb');
 
 // Connessione al DB
 const db = new sqlite3.Database('/tmp/database.db', (err) => {
